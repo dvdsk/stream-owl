96022a19b52181d7689b8cb657e0f8a32793a3ea
dvdsk <noreply@davidsk.dev>
2024-10-31 16:46:38 +0100

    turn stream example into an actual example instead of test

--- examples/stream.rs
+++ examples/stream.rs
@@ -1,36 +1,35 @@
-#![recursion_limit = "256"]
-
+// #![recursion_limit = "256"]
 use std::error::Error;
 use std::io::Read;
 use std::num::NonZeroUsize;
-use std::sync::Arc;
 
-use stream_owl::StreamBuilder;
-use stream_owl_test_support::tracing_setup;
-use stream_owl_test_support::{setup_reader_test, static_file_server};
-use tokio::sync::Notify;
+use tokio::task;
 
-fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-    let _guard = tracing_setup::opentelemetry();
+// 274- The Age of the Algorithm;
+const URL1: &str = "https://dts.podtrac.com/redirect.mp3/chrt.fm/track/288D49/stitcher.simplecastaudio.com/3bb687b0-04af-4257-90f1-39eef4e631b6/episodes/c660ce6b-ced1-459f-9535-113c670e83c9/audio/128/default.mp3?aid=rss_feed&awCollectionId=3bb687b0-04af-4257-90f1-39eef4e631b6&awEpisodeId=c660ce6b-ced1-459f-9535-113c670e83c9&feed=BqbsxVfO";
 
-    let configure = {
-        move |b: StreamBuilder<false, _, _, _>| {
-            b.with_prefetch(0)
-                .to_unlimited_mem()
-                .with_fixed_chunk_size(NonZeroUsize::new(10_000).unwrap())
-        }
-    };
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    let (mut handle, stream) = stream_owl::StreamBuilder::try_new(URL1)
+        .unwrap()
+        .with_prefetch(0)
+        .to_unlimited_mem()
+        .with_fixed_chunk_size(NonZeroUsize::new(10_000).unwrap())
+        .start()
+        .await
+        .unwrap();
 
-    let test_done = Arc::new(Notify::new());
-    let (_runtime_thread, mut handle) = {
-        setup_reader_test(&test_done, 100_000, configure, move |size| {
-            static_file_server(size)
-        })
-    };
+    task::spawn(stream);
 
-    let mut reader = handle.try_get_reader()?;
-    reader.read_exact(&mut vec![0; 100_000])?;
-    test_done.notify_one();
+    let mut reader = handle
+        .try_get_reader()
+        .expect("reader has not yet been taken");
+
+    let _ = task::spawn_blocking(move || {
+        let mut buf = vec![0u8; 100_000];
+        reader.read(&mut buf).unwrap();
+        assert!(buf.iter().filter(|i| **i == 0).count() < 100);
+    });
 
     Ok(())
 }
